fb71ddceb8d8e88d6853af5ac0c8eedddbba04ae
Saqib Ahmed <saqib.ahmed8p2@gmail.com>
2020-08-10 12:55:27 +0500

    add function add screenshot and videos of failed cases report publish on gh-pages

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -8,20 +8,10 @@ Cypress.on('test:after:run', (test, runnable) => {
 
    if (test.state === 'failed') {
       const filename = `${titleToFileName(runnable.parent.title)} -- ${titleToFileName(test.title)} (failed).png`;
-      addContext({ test }, `../screenshots/${Cypress.spec.name}/${filename}`);
-      //addContext({ test }, `../videos/${Cypress.spec.name}.mp4`);
-
-      let videoName = Cypress.spec.name
-      videoName = videoName.replace('/.js.*', '.js')
-      const videoUrl = 'videos/' + videoName + '.mp4'
-      addContext({ test }, videoUrl)
+      addContext({ test }, `screenshots/${Cypress.spec.name}/${filename}`);
+      addContext({ test }, `videos/${Cypress.spec.name}.mp4`);
    } else if (test.state === 'pass'){
-
-      addContext({ test }, `../videos/${Cypress.spec.name}.mp4`);
-      // let videoName = Cypress.spec.name
-      // videoName = videoName.replace('/.js.*', '.js')
-      // const videoUrl = 'videos/' + videoName + '.mp4'
-      // addContext({ test }, videoUrl)
+      addContext({ test }, `videos/${Cypress.spec.name}.mp4`);
    }
 });
 

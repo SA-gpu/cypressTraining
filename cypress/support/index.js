cde15c8265757aa2b9ac3074100faba35afd64c5
Saqib Ahmed <saqib.ahmed8p2@gmail.com>
2020-08-10 12:38:44 +0500

    on failed video attached

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,5 +1,30 @@
 import './commands'
 require('cypress-plugin-retries')
+import addContext from 'mochawesome/addContext';
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+const titleToFileName = (title) => title.replace(/[:\/]/g, '');
+
+Cypress.on('test:after:run', (test, runnable) => {
+
+   if (test.state === 'failed') {
+      const filename = `${titleToFileName(runnable.parent.title)} -- ${titleToFileName(test.title)} (failed).png`;
+      addContext({ test }, `../screenshots/${Cypress.spec.name}/${filename}`);
+      //addContext({ test }, `../videos/${Cypress.spec.name}.mp4`);
+
+      let videoName = Cypress.spec.name
+      videoName = videoName.replace('/.js.*', '.js')
+      const videoUrl = 'videos/' + videoName + '.mp4'
+      addContext({ test }, videoUrl)
+   } else if (test.state === 'pass'){
+
+      addContext({ test }, `../videos/${Cypress.spec.name}.mp4`);
+      // let videoName = Cypress.spec.name
+      // videoName = videoName.replace('/.js.*', '.js')
+      // const videoUrl = 'videos/' + videoName + '.mp4'
+      // addContext({ test }, videoUrl)
+   }
+});
+
+
+
+
